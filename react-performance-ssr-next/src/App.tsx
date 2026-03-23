c78068d771c3abf4c52b450fb70c12410fed7850
Regina Frontend <reginakarimovna1330@gmail.com>
2026-03-23 14:46:14 +0600

    feat: optimize app with lazy loading and code splitting

--- react-performance-ssr-next/src/App.tsx
+++ react-performance-ssr-next/src/App.tsx
@@ -1,23 +1,26 @@
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import CusomLayout from "./components/Layout/Layout";
+import { lazy, Suspense } from "react";
 
-import HomePage from "./pages/HomePage/HomePage";
-import PostsPage from "./pages/PostsPage/PostsPage";
-import AlbumsPage from "./pages/AlbumsPage/AlbumsPage";
-import TodoPage from "./pages/TodoPage/TodoPage";
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const PostsPage = lazy(() => import("./pages/PostsPage/PostsPage"));
+const AlbumsPage = lazy(() => import("./pages/AlbumsPage/AlbumsPage"));
+const TodoPage = lazy(() => import("./pages/TodoPage/TodoPage"));
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CusomLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="posts" element={<PostsPage />} />
-          <Route path="albums" element={<AlbumsPage />} />
-          <Route path="todos" element={<TodoPage />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Загрузка...</div>}>
+        <Routes>
+          <Route path="/" element={<CusomLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="posts" element={<PostsPage />} />
+            <Route path="albums" element={<AlbumsPage />} />
+            <Route path="todos" element={<TodoPage />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
